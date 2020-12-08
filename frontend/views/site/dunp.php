cdb7507890fbb79ec5bdc97b8f7164883d4a9708
ValentineWa <waweruval@gmail.com>
2020-12-08 17:35:08 +0300

    dump code

--- frontend/views/site/dunp.php
+++ frontend/views/site/dunp.php
@@ -486,3 +486,65 @@ $cartPrice = Cart::find()->joinWith('shoes')->sum('price');
 <a href="<?= Url::to(['shoes/addtocart', 'shoesId'=>$shoez->shoesId])?>" class="btn btn-outline-info btitley" role="button">Add to Cart</a>
 <a href="<?= Url::to(['shoes/addtocart', 'shoesId'=>$shoez->shoesId])?>" class="btn btn-outline-info btitley" role="button">Add to Cart</a>
 <a href="<?= Url::to(['shoes/addtocart', 'shoesId'=>$shoez->shoesId])?>" class="btn btn-outline-info btitley" role="button">Add to Cart</a>
+
+
+
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Cart */
+/* @var $form ActiveForm */
+
+?>
+<div class="addtocart">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'shoesId') ?>
+        <?= $form->field($model, 'userId') ?>
+    
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div>
+'shoe
+
+
+
+
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use frontend\models\Shoes;
+use common\models\User;
+use yii\helpers\ArrayHelper;
+use\yii\helpers\Url;
+use frontend\models\Images;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Cart */
+/* @var $form ActiveForm */
+$shoes = Shoes::find()->JoinWith('shoesimages')->all();
+$shoe = ArrayHelper::map(Shoes::find()->all(), 'shoesId', 'price');
+$userId = User::find()->where(['id'=>Yii::$app->user->id])->one();
+
+?>
+<div class="addtocart">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'shoesId')->hiddenInput(['value' => $shoesId, 'readonly'=> true])->label(false) ?>
+        <?= $form->field($model, 'userId')->hiddenInput(['value'=>$userId->id, 'readonly'=> true])->label(false) ?>
+    
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- addtocart -->
